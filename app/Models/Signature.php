98fb40491ef9a0dec9c49c7671de76e408a0b4a3
Davson Santos <davsonsantos@gmail.com>
2024-07-08 16:33:03 -0400

    models

--- app/Models/Signature.php
+++ app/Models/Signature.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use App\Enums\SignatureStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Signature extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['client_id', 'plan_id', 'status'];
+    protected $cast = [
+        'status' => SignatureStatus::class
+    ];
 }
